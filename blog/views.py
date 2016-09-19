ee9e1a3731d1e081cf3843f59beb4cc62690fd8a
Kelly Wright <kelly.wright@kellyinnovation.com>
2016-09-19 13:52:52 -0400

    View blog posts.

--- blog/views.py
+++ blog/views.py
@@ -9,3 +9,10 @@ def index(request):
 		"posts": posts
 	}
 	return render(request, "blog/index.html", context)
+
+def post(request, id):
+	post = Post.objects.get(pk=id)
+	context = {
+		"post": post
+	}
+	return render(request, "blog/post.html", context)
